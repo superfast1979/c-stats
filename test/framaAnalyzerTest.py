fae77542d719856495336e025d9920ce876db431
augello <augello@itmi00dw1427.corp.dom>
2018-07-14 16:40:38 +0200

    Refactoring test

--- test/framaAnalyzerTest.py
+++ test/framaAnalyzerTest.py
@@ -5,10 +5,11 @@ import framaAnalyzer as fa
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
-class Test_framaStats(unittest.TestCase):
+class Test_framaAnalyzer(unittest.TestCase):
 
     def setUp(self):
         self.createFramaAnalyzer()
+        self.slocAnalyzer = self.siObj.getFramaSlocAnalyzer()
 
     def tearDown(self):
         pass
@@ -28,11 +29,9 @@ class Test_framaStats(unittest.TestCase):
         self.assertEqual(79, self.createFramaAnalyzer())
         
     def test_getFunctionList(self):
-        self.createFramaAnalyzer()
         self.assertEqual(79, len(self.siObj.getFunctionObjectList()))
         
     def test_getFunctionListCheckObject(self):
-        self.createFramaAnalyzer()
         funcList = self.siObj.getFunctionObjectList()
         self.assertEqual("check_num_record", funcList[0].getName())
         self.assertEqual(2, funcList[0].getSloc())
@@ -41,143 +40,118 @@ class Test_framaStats(unittest.TestCase):
         self.assertEqual(28, funcList[78].getSloc())
         self.assertEqual(7, funcList[78].getMccabeComplexity())
 
-    def test_getTotalSloc(self):
+
+class Test_framaSlocAnalyzer(unittest.TestCase):
+
+    def setUp(self):
         self.createFramaAnalyzer()
-        slocAnalyzer = self.siObj.getFramaSlocAnalyzer()
-        self.assertEqual(1567, slocAnalyzer.getTotalSloc())
+        self.slocAnalyzer = self.siObj.getFramaSlocAnalyzer()
+
+    def tearDown(self):
+        pass
+
+    def createFramaAnalyzer(self):
+        self.siObj = fa.framaAnalyzer(os.path.join(ROOT_DIR, 'pippo.txt'))
+        return self.siObj.extractSectionsFromFile()
+
+    def test_getTotalSloc(self):
+        self.assertEqual(1567, self.slocAnalyzer.getTotalSloc())
         
     def test_getTotalSlocOver60(self):
-        self.createFramaAnalyzer()
-        slocAnalyzer = self.siObj.getFramaSlocAnalyzer()
-        self.assertEqual(333, slocAnalyzer.getTotalSlocOver60())
+        self.assertEqual(333, self.slocAnalyzer.getTotalSlocOver60())
         
     def test_getPercentageSlocOver60(self):
-        self.createFramaAnalyzer()
-        slocAnalyzer = self.siObj.getFramaSlocAnalyzer()
-        self.assertEqual(21.25, slocAnalyzer.getPercentageSlocOver60())
+        self.assertEqual(21.25, self.slocAnalyzer.getPercentageSlocOver60())
         
     def test_getTotalSloc30To60(self):
-        self.createFramaAnalyzer()
-        slocAnalyzer = self.siObj.getFramaSlocAnalyzer()
-        self.assertEqual(290, slocAnalyzer.getTotalSloc30To60())
+        self.assertEqual(290, self.slocAnalyzer.getTotalSloc30To60())
         
     def test_getPercentageSloc30To60(self):
-        self.createFramaAnalyzer()
-        slocAnalyzer = self.siObj.getFramaSlocAnalyzer()
-        self.assertEqual(18.51, slocAnalyzer.getPercentageSloc30To60())
+        self.assertEqual(18.51, self.slocAnalyzer.getPercentageSloc30To60())
         
     def test_getTotalSloc15To30(self):
-        self.createFramaAnalyzer()
-        slocAnalyzer = self.siObj.getFramaSlocAnalyzer()
-        self.assertEqual(642, slocAnalyzer.getTotalSloc15To30())
+        self.assertEqual(642, self.slocAnalyzer.getTotalSloc15To30())
         
     def test_getPercentageSloc15To30(self):
-        self.createFramaAnalyzer()
-        slocAnalyzer = self.siObj.getFramaSlocAnalyzer()
-        self.assertEqual(40.97, slocAnalyzer.getPercentageSloc15To30())
+        self.assertEqual(40.97, self.slocAnalyzer.getPercentageSloc15To30())
         
     def test_getTotalSlocUnder15(self):
-        self.createFramaAnalyzer()
-        slocAnalyzer = self.siObj.getFramaSlocAnalyzer()
-        self.assertEqual(302, slocAnalyzer.getTotalSlocUnder15())
+        self.assertEqual(302, self.slocAnalyzer.getTotalSlocUnder15())
         
     def test_getPercentageSlocUnder15(self):
-        self.createFramaAnalyzer()
-        slocAnalyzer = self.siObj.getFramaSlocAnalyzer()
-        self.assertEqual(19.27, slocAnalyzer.getPercentageSlocUnder15())
+        self.assertEqual(19.27, self.slocAnalyzer.getPercentageSlocUnder15())
 
     def test_sumAllPercentage(self):
-        self.createFramaAnalyzer()
-        slocAnalyzer = self.siObj.getFramaSlocAnalyzer()
-        sumPerc = slocAnalyzer.getPercentageSlocUnder15()
-        sumPerc = sumPerc + slocAnalyzer.getPercentageSloc15To30()
-        sumPerc = sumPerc + slocAnalyzer.getPercentageSloc30To60()
-        sumPerc = sumPerc + slocAnalyzer.getPercentageSlocOver60()
+        sumPerc = self.slocAnalyzer.getPercentageSlocUnder15()
+        sumPerc = sumPerc + self.slocAnalyzer.getPercentageSloc15To30()
+        sumPerc = sumPerc + self.slocAnalyzer.getPercentageSloc30To60()
+        sumPerc = sumPerc + self.slocAnalyzer.getPercentageSlocOver60()
         self.assertEqual(100, sumPerc)
         
     def test_getCutoff(self):
-        self.createFramaAnalyzer()
-        slocAnalyzer = self.siObj.getFramaSlocAnalyzer()
-        self.assertEqual([5, 16, 31, 69], slocAnalyzer.getCutoff('fiveStar'))
-        self.assertEqual([7, 22, 44, 56], slocAnalyzer.getCutoff('fourStar'))
-        self.assertEqual([9, 28, 55, 45], slocAnalyzer.getCutoff('threeStar'))
-        self.assertEqual([10, 34, 67, 33], slocAnalyzer.getCutoff('twoStar'))
+        self.assertEqual([5, 16, 31, 69], self.slocAnalyzer.getCutoff('fiveStar'))
+        self.assertEqual([7, 22, 44, 56], self.slocAnalyzer.getCutoff('fourStar'))
+        self.assertEqual([9, 28, 55, 45], self.slocAnalyzer.getCutoff('threeStar'))
+        self.assertEqual([10, 34, 67, 33], self.slocAnalyzer.getCutoff('twoStar'))
         
     def test_isOverWorstThreshold(self):
-        self.createFramaAnalyzer()
-        slocAnalyzer = self.siObj.getFramaSlocAnalyzer()
-        self.assertTrue(slocAnalyzer.isOverWorstThreshold([9, 0, 0, 91], [7, 22, 44, 56]))
-        self.assertFalse(slocAnalyzer.isOverWorstThreshold([7, 0, 0, 91], [7, 22, 44, 56]))
-        self.assertFalse(slocAnalyzer.isOverWorstThreshold([6, 0, 0, 91], [7, 22, 44, 56]))
-        self.assertTrue(slocAnalyzer.isOverWorstThreshold([6, 0, 0, 91], [5, 22, 44, 56]))
+        self.assertTrue(self.slocAnalyzer.isOverWorstThreshold([9, 0, 0, 91], [7, 22, 44, 56]))
+        self.assertFalse(self.slocAnalyzer.isOverWorstThreshold([7, 0, 0, 91], [7, 22, 44, 56]))
+        self.assertFalse(self.slocAnalyzer.isOverWorstThreshold([6, 0, 0, 91], [7, 22, 44, 56]))
+        self.assertTrue(self.slocAnalyzer.isOverWorstThreshold([6, 0, 0, 91], [5, 22, 44, 56]))
         
     def test_isOverSecondThreshold(self):
-        self.createFramaAnalyzer()
-        slocAnalyzer = self.siObj.getFramaSlocAnalyzer()
-        self.assertTrue(slocAnalyzer.isOverSecondThreshold([5, 18, 0, 91], [7, 22, 44, 56]))
-        self.assertTrue(slocAnalyzer.isOverSecondThreshold([4, 19, 0, 91], [7, 22, 44, 56]))
-        self.assertFalse(slocAnalyzer.isOverSecondThreshold([5, 17, 0, 91], [7, 22, 44, 56]))
-        self.assertFalse(slocAnalyzer.isOverSecondThreshold([4, 18, 0, 91], [7, 22, 44, 56]))
+        self.assertTrue(self.slocAnalyzer.isOverSecondThreshold([5, 18, 0, 91], [7, 22, 44, 56]))
+        self.assertTrue(self.slocAnalyzer.isOverSecondThreshold([4, 19, 0, 91], [7, 22, 44, 56]))
+        self.assertFalse(self.slocAnalyzer.isOverSecondThreshold([5, 17, 0, 91], [7, 22, 44, 56]))
+        self.assertFalse(self.slocAnalyzer.isOverSecondThreshold([4, 18, 0, 91], [7, 22, 44, 56]))
         
     def test_isOverThirdThreshold(self):
-        self.createFramaAnalyzer()
-        slocAnalyzer = self.siObj.getFramaSlocAnalyzer()
-        self.assertTrue(slocAnalyzer.isOverThirdThreshold([5, 17, 23, 91], [7, 22, 44, 56]))
-        self.assertTrue(slocAnalyzer.isOverThirdThreshold([7, 15, 23, 91], [7, 22, 44, 56]))
-        self.assertTrue(slocAnalyzer.isOverThirdThreshold([1, 1, 43, 91], [7, 22, 44, 56]))
-        self.assertFalse(slocAnalyzer.isOverThirdThreshold([4, 18, 22, 91], [7, 22, 44, 56]))
+        self.assertTrue(self.slocAnalyzer.isOverThirdThreshold([5, 17, 23, 91], [7, 22, 44, 56]))
+        self.assertTrue(self.slocAnalyzer.isOverThirdThreshold([7, 15, 23, 91], [7, 22, 44, 56]))
+        self.assertTrue(self.slocAnalyzer.isOverThirdThreshold([1, 1, 43, 91], [7, 22, 44, 56]))
+        self.assertFalse(self.slocAnalyzer.isOverThirdThreshold([4, 18, 22, 91], [7, 22, 44, 56]))
         
     def test_isOneRateStars(self):
-        self.createFramaAnalyzer()
-        slocAnalyzer = self.siObj.getFramaSlocAnalyzer()
-        self.assertTrue(slocAnalyzer.isOneRateStars([11, 0, 0, 91]))
-        self.assertTrue(slocAnalyzer.isOneRateStars([9, 36, 0, 91]))
-        self.assertFalse(slocAnalyzer.isOneRateStars([10, 24, 23, 33]))
+        self.assertTrue(self.slocAnalyzer.isOneRateStars([11, 0, 0, 91]))
+        self.assertTrue(self.slocAnalyzer.isOneRateStars([9, 36, 0, 91]))
+        self.assertFalse(self.slocAnalyzer.isOneRateStars([10, 24, 23, 33]))
 
     def test_isTwoRateStars(self):
-        self.createFramaAnalyzer()
-        slocAnalyzer = self.siObj.getFramaSlocAnalyzer()
-        self.assertFalse(slocAnalyzer.isTwoRateStars([9, 0, 0, 91]))
-        self.assertFalse(slocAnalyzer.isTwoRateStars([7, 0, 0, 91]))
-        self.assertFalse(slocAnalyzer.isTwoRateStars([1, 7, 19, 81]))
-        self.assertFalse(slocAnalyzer.isTwoRateStars([1, 9, 0, 91]))
-        self.assertTrue(slocAnalyzer.isTwoRateStars([10, 24, 23, 80]))
+        self.assertFalse(self.slocAnalyzer.isTwoRateStars([9, 0, 0, 91]))
+        self.assertFalse(self.slocAnalyzer.isTwoRateStars([7, 0, 0, 91]))
+        self.assertFalse(self.slocAnalyzer.isTwoRateStars([1, 7, 19, 81]))
+        self.assertFalse(self.slocAnalyzer.isTwoRateStars([1, 9, 0, 91]))
+        self.assertTrue(self.slocAnalyzer.isTwoRateStars([10, 24, 23, 80]))
 
     def test_isThreeRateStars(self):
-        self.createFramaAnalyzer()
-        slocAnalyzer = self.siObj.getFramaSlocAnalyzer()
-        self.assertTrue(slocAnalyzer.isThreeRateStars([9, 0, 0, 91]))
-        self.assertFalse(slocAnalyzer.isThreeRateStars([7, 0, 0, 91]))
-        self.assertFalse(slocAnalyzer.isThreeRateStars([1, 9, 0, 91]))
-        self.assertFalse(slocAnalyzer.isThreeRateStars([1, 7, 12, 80]))
-        self.assertFalse(slocAnalyzer.isThreeRateStars([1, 7, 19, 81]))
-        self.assertTrue(slocAnalyzer.isThreeRateStars([4, 23, 28, 80]))
-        self.assertTrue(slocAnalyzer.isThreeRateStars([9, 19, 23, 80]))
+        self.assertTrue(self.slocAnalyzer.isThreeRateStars([9, 0, 0, 91]))
+        self.assertFalse(self.slocAnalyzer.isThreeRateStars([7, 0, 0, 91]))
+        self.assertFalse(self.slocAnalyzer.isThreeRateStars([1, 9, 0, 91]))
+        self.assertFalse(self.slocAnalyzer.isThreeRateStars([1, 7, 12, 80]))
+        self.assertFalse(self.slocAnalyzer.isThreeRateStars([1, 7, 19, 81]))
+        self.assertTrue(self.slocAnalyzer.isThreeRateStars([4, 23, 28, 80]))
+        self.assertTrue(self.slocAnalyzer.isThreeRateStars([9, 19, 23, 80]))
          
     def test_isFourRateStars(self):
-        self.createFramaAnalyzer()
-        slocAnalyzer = self.siObj.getFramaSlocAnalyzer()
-        self.assertFalse(slocAnalyzer.isFourRateStars([9, 0, 0, 91]))
-        self.assertFalse(slocAnalyzer.isFourRateStars([1, 9, 0, 91]))
-        self.assertFalse(slocAnalyzer.isFourRateStars([1, 7, 12, 80]))
-        self.assertFalse(slocAnalyzer.isFourRateStars([1, 7, 19, 81]))
-        self.assertFalse(slocAnalyzer.isFourRateStars([4, 12, 15, 80]))
-        self.assertFalse(slocAnalyzer.isFourRateStars([1, 15, 10, 80]))
-        self.assertTrue(slocAnalyzer.isFourRateStars([7, 0, 0, 91]))
-        self.assertTrue(slocAnalyzer.isFourRateStars([7, 10, 26, 60]))
+        self.assertFalse(self.slocAnalyzer.isFourRateStars([9, 0, 0, 91]))
+        self.assertFalse(self.slocAnalyzer.isFourRateStars([1, 9, 0, 91]))
+        self.assertFalse(self.slocAnalyzer.isFourRateStars([1, 7, 12, 80]))
+        self.assertFalse(self.slocAnalyzer.isFourRateStars([1, 7, 19, 81]))
+        self.assertFalse(self.slocAnalyzer.isFourRateStars([4, 12, 15, 80]))
+        self.assertFalse(self.slocAnalyzer.isFourRateStars([1, 15, 10, 80]))
+        self.assertTrue(self.slocAnalyzer.isFourRateStars([7, 0, 0, 91]))
+        self.assertTrue(self.slocAnalyzer.isFourRateStars([7, 10, 26, 60]))
  
     def test_isFiveRateStars(self):
-        self.createFramaAnalyzer()
-        slocAnalyzer = self.siObj.getFramaSlocAnalyzer()
-        self.assertFalse(slocAnalyzer.isFiveRateStars([9, 0, 0, 91]))
-        self.assertTrue(slocAnalyzer.isFiveRateStars([5, 0, 0, 91]))
+        self.assertFalse(self.slocAnalyzer.isFiveRateStars([9, 0, 0, 91]))
+        self.assertTrue(self.slocAnalyzer.isFiveRateStars([5, 0, 0, 91]))
           
     def test_getRateStars(self):
-        self.createFramaAnalyzer()
-        slocAnalyzer = self.siObj.getFramaSlocAnalyzer()
-        self.assertEqual(1, slocAnalyzer.calculateRateStars())
+        self.assertEqual(1, self.slocAnalyzer.calculateRateStars())
 
 
-suite = unittest.TestLoader().loadTestsFromTestCase(Test_framaStats)
+suite = unittest.TestLoader().loadTestsFromTestCase(Test_framaAnalyzer)
+suite = unittest.TestLoader().loadTestsFromTestCase(Test_framaSlocAnalyzer)
 unittest.TextTestRunner(verbosity=2).run(suite)
 
